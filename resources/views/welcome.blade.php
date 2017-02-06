a53e95fc38e51ef4d29d5e280fb24d02ed35f3ce
Mohamed Bareche <Mohamed Bareche>
2017-02-06 08:55:22 +0100

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 2 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Thesis streamin application">
+    <meta name="author" content="mmdBareche">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Twithese</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom style -->
+    <link href="../css/app.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Twithese</h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="#">Accueil</a></li>
+                  <li><a href="#">Fonctionnalités</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Enregistrer votre thèse.</h1>
+            <p class="lead">Twithese vous permet de programmer l'enregistrement audio et vidéo de votre these blablabla .</p>
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-default">Se connecter</a>
+            </p>
+          </div>
+
+            <div class="mastfoot">
+            <div class="inner">
+              <p>TER réalisé par by MBareche, CMceski, KChabane.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../js/bootstrap.min.js"></script>
+  </body>
+</html>
